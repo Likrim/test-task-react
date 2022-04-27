30dd2ecab740c004d727f9c14091a8f3e73e46a0
Oleksandr Kozak <sashakozak073@gmail.com>
2022-04-27 18:42:14 +0300

    change elements to functional

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 import logo from './img/logo.jpg';
@@ -12,151 +12,117 @@ function sortByLastName(a, b) {
     return 0;
 }
 
-class HeaderSite extends React.Component {
-    render() {
-        return (
-            <div className="HeaderSite">
-                <img className="Logo" src={logo}/>
-                <div className="HeadText">User Profiles</div>
-            </div>
-        );
-    }
+const HeaderSite = () => {
+    return (
+        <div className="HeaderSite">
+            <img className="Logo" src={logo}/>
+            <div className="HeadText">User Profiles</div>
+        </div>
+    );
 }
 
-class UserButton extends React.Component {
-    constructor(props){
-        super(props);
-        this.handleOnClick = this.handleOnClick.bind(this);
+const UserButton = (props) => {
+    function handleOnClick(){
+        props.onClick(props.user);
     }
 
-    handleOnClick() {
-        this.props.onClick(this.props.user);
-    }
-
-    render() {
-        const fullName = this.props.user.firstName + " " + this.props.user.lastname;
-        return (
-            <tr>
-                <td>
-                    <button onClick={this.handleOnClick} className="UserButton">
-                        {fullName}
-                    </button>
-                </td>
-            </tr>
-        );
-    }
+    const fullName = props.user.firstName + " " + props.user.lastname;
+    return (
+        <tr>
+            <td>
+                <button onClick={handleOnClick} className="UserButton">
+                    {fullName}
+                </button>
+            </td>
+        </tr>
+    );
 }
 
-class InformationBlock extends React.Component {
-    render() {
-        return (
-            <table className="InformationTable">
-                <thead>
-                    <tr>
-                        <th colSpan={5} align="center">Information</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th className="InformationTableTH">First Name</th>
-                        <th className="InformationTableTH">Last Name</th>
-                        <th className="InformationTableTH">Age</th>
-                        <th className="InformationTableTH">Phone</th>
-                        <th className="InformationTableTH">Role</th>
-                    </tr>
-                    <tr>
-                        <td className="InformationTableTD">{this.props.user.firstName}</td>
-                        <td className="InformationTableTD">{this.props.user.lastname}</td>
-                        <td className="InformationTableTD">{this.props.user.age}</td>
-                        <td className="InformationTableTD">{this.props.user.phone}</td>
-                        <td className="InformationTableTD">{this.props.user.role}</td>
-                    </tr>
-                </tbody>
-            </table>
-        );
-    }
+const InformationBlock = (props) => {
+    return (
+        <table className="InformationTable">
+            <thead>
+                <tr>
+                    <th colSpan={5} align="center">Information</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th className="InformationTableTH">First Name</th>
+                    <th className="InformationTableTH">Last Name</th>
+                    <th className="InformationTableTH">Age</th>
+                    <th className="InformationTableTH">Phone</th>
+                    <th className="InformationTableTH">Role</th>
+                </tr>
+                <tr>
+                    <td className="InformationTableTD">{props.user.firstName}</td>
+                    <td className="InformationTableTD">{props.user.lastname}</td>
+                    <td className="InformationTableTD">{props.user.age}</td>
+                    <td className="InformationTableTD">{props.user.phone}</td>
+                    <td className="InformationTableTD">{props.user.role}</td>
+                </tr>
+            </tbody>
+        </table>
+    );
 }
 
-class UserProfilesTable extends React.Component {
-    render() {
-        const rows = [];
+const UserProfilesTable = (props) => {
+    const rows = [];
 
-        const users = this.props.users;
-        users.sort(sortByLastName);
-        users.forEach((user) => {
-            rows.push(
-                <UserButton 
-                user={user}
-                onClick={this.props.onClick} 
-                key={user.id}/>
-            );
-        });
-        return (
-            <table>
-                <thead>
-                    <tr align="center">
-                        <th className="TableHeadText">User</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows}
-                </tbody>
-            </table>
+    const users = props.users;
+    users.sort(sortByLastName);
+    users.forEach((user) => {
+        rows.push(
+            <UserButton 
+            user={user}
+            onClick={props.onClick} 
+            key={user.id}/>
         );
-    }
+    });
+
+    return (
+        <table>
+            <thead>
+                <tr align="center">
+                    <th className="TableHeadText">User</th>
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </table>
+    );
 }
 
-class UserProfiles extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleOnClick = this.handleOnClick.bind(this);
-        this.state = {
-            isClicked: false,
-            id: -1,
-            user: {
-                id: -1,
-                firstName: "",
-                lastname: "",
-                age: -1,
-                phone: "",
-                role: ""
-            }
-        };
-    }
+const UserProfiles = (props) => {
+    const [isClicked, setIsClicked] = useState(false);
+    const [id, setId] = useState(-1);
+    const [user, setUser] = useState({id: -1, firstName: '', lastname: '', age: -1, phone: '', role: ''});
 
-    handleOnClick(user) {
-        if(this.state.isClicked && this.state.id !== user.id){
-            this.setState({
-                id: user.id,
-                user: user
-            });
-        } 
-        else if(this.state.isClicked && this.state.id === user.id) {
-            this.setState({
-                isClicked: false
-            })
-        } else if(!this.state.isClicked) {
-            this.setState({
-                isClicked: true,
-                id: user.id,
-                user: user
-            });
+    function handleOnClick(user) {
+        if(isClicked && id !== user.id) {
+            setId(user.id);
+            setUser(user);
+        } else if(isClicked && id === user.id) {
+            setIsClicked(false);
+        } else if(!isClicked) {
+            setIsClicked(true);
+            setId(user.id);
+            setUser(user);
         }
     }
 
-    render() {
-        return (
-            <div className="MainBlock">
-                <HeaderSite />
-                <br/>
-                <UserProfilesTable 
-                onClick={this.handleOnClick}
-                users={this.props.users}/>
-                <br/>
-                {this.state.isClicked && <InformationBlock user={this.state.user}/>}
-            </div>
-        );
-    }
+    return(
+        <div className="MainBlock">
+            <HeaderSite />
+            <br/>
+            <UserProfilesTable 
+            onClick={handleOnClick}
+            users={props.users}/>
+            <br/>
+            {isClicked && <InformationBlock user={user}/>}
+        </div>
+    );
 }
 
 ReactDOM.render(
